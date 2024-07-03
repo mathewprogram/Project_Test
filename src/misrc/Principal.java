d676f53fae36e7fc77419f1e118dd2573de7d0c1
mathewprogram <ionitza.matei@icloud.com>
2024-07-03 10:25:34 +0200

    Version 1.4 cambios realizados en la rama1

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -1,11 +1,15 @@
 package misrc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Principal {
 
     public static void main(String[] args) {
-        List<String> nombres_al = null;
+        List<String> nombres_al = new ArrayList<>();
+        
+        nombres_al.add("Luis");
+        nombres_al.add("Miguel");
         
         for(String elementos: nombres_al){
             System.out.println(elementos);
