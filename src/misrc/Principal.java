503de16ea3f82b70cd84c5cb08e406fc13e8f6e7
mathewprogram <ionitza.matei@icloud.com>
2024-07-03 09:35:18 +0200

    Version 1.1

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -3,7 +3,7 @@ package misrc;
 public class Principal {
 
     public static void main(String[] args) {
-        
+        //este es un comentario para diferenciar la version 1 de version 1.1
     }
     
 }
