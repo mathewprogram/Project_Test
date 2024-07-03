9c1ce79f5fcfddf306e8d7e541812ed5fcd3d21c
mathewprogram <ionitza.matei@icloud.com>
2024-07-03 09:40:04 +0200

    Version 1.2

--- src/misrc/Principal.java
+++ src/misrc/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
     public static void main(String[] args) {
         //este es un comentario para diferenciar la version 1 de version 1.1
+        System.out.println("Hola Git");
     }
     
 }
